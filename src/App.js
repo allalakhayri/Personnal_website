a88d8c3e6269bb6f3fc18ad8fce3d5fcfeaeb0da
khayri <khayri.allala2018@gmai.com>
2023-03-29 12:08:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div >
-    <p>  Hello everyone, what's up man ? am good </p>
+    <div className="App">
+      <header className="App-header">
+    
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
