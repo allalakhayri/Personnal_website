d82787ef269ec60455621fbdf62d11e7d3404f34
ALLALA Mohamed Khayri <90345147+allalakhayri@users.noreply.github.com>
2023-03-29 12:29:32 +0100

    commit App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import './App.css';
+import {Header} from'./components/Header'
+import { HomeStart } from './components/Home'
+import { About } from './components/About'
+import { Services } from './components/Services'
+import {Skills} from'./components/Skills'
+import { Contact } from './components/Contact'
+import { Certificates } from './components/Certificates'
+import { Projects } from './components/Projects'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Footer} from'./components/Footer'
+import React from 'react'
+const images = [
+  "../images/pic1.jpg",
+  "../images/pic3.jpg",
+  "../images/pic4.jpg",
+  "../images/pic5.jpg",
+  "../images/pic6.jpg",
+  "../images/pic7.jpg"
+];
+
+ const App = ()=>{
+    return (
+ <div>
+  <div className="scroll-up-btn">
+    <i className="fas fa-angle-up" />
+  </div>
+
+  <Header/>
+  <HomeStart/>
+  <About/>
+  <Skills /> 
+  <Services/>
+  <Certificates images={images}/>
+  <Projects/>
+  <Contact/>
+  <Footer/> 
+  
+ </div> 
+    )
 }
-
-export default App;
+export default App
