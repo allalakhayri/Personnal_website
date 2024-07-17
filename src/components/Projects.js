d4e44b7eb5aea5d4021b751651a2c6f9bbd2215d
khayri <khayri.allala2018@gmai.com>
2024-07-17 16:04:53 +0100

    new version

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../App.css';
 
 export const Projects = () => {
-  const [scrollPosition] = useState(0);
   const scrollContainerRef = React.createRef();
 
   const projects = [
     {
       title: 'Monopoly Game',
-      image: '../images/monopoly.png', 
+      image: '../images/monopoly.png',
       description: 'A famous social game developed using Qt as part of a summer project at my school.',
       keywords: 'C++, Qt, GUI',
       github: 'https://github.com/allalakhayri/Monopoly-Game',
-       
     },
     {
       title: 'Tunijob Platform',
@@ -20,7 +18,6 @@ export const Projects = () => {
       description: 'A Tunisian platform connecting job providers with job seekers.',
       keywords: 'Web dev, MERN stack',
       github: 'https://github.com/allalakhayri/Ensi-project-2022',
-     
     },
     {
       title: 'FreelanceFlow App',
@@ -28,29 +25,28 @@ export const Projects = () => {
       description: 'An Android app connecting freelancers with clients.',
       keywords: 'Mobile dev, Android, Kotlin',
       github: 'https://github.com/allalakhayri/FreelanceFlow',
-    
     },
     {
       title: 'AI-powered Document Management System',
-      image: '../images/home_screen.PNG', 
+      image: '../images/home_screen.PNG',
       description: 'The Intelligent Document Management System aims to revolutionize document management using advanced artificial intelligence technologies.',
       keywords: 'AI, Serverless Architecture, Firebase, OCR',
-      demo: 'https://drive.google.com/drive/folders/1eMc_1bTmdrYYqePDPLvm6pUwCo-feizS', 
+      demo: 'https://drive.google.com/drive/folders/1eMc_1bTmdrYYqePDPLvm6pUwCo-feizS',
     },
     {
       title: 'TV Channel Mobile App',
-      image: '../images/alhiwar.jpg', 
+      image: '../images/alhiwar.jpg',
       description: 'This mobile app is a comprehensive news platform for a TV channel, providing users with real-time news updates and video streaming.',
       keywords: 'Mobile dev, Flutter, Firebase',
-      demo: 'https://drive.google.com/file/d/1qdqUnVZts6A-fa2icuLpi_um8tO3K_P_/view?usp=sharing', 
+      demo: 'https://drive.google.com/file/d/1qdqUnVZts6A-fa2icuLpi_um8tO3K_P_/view?usp=sharing',
     },
     {
       title: 'Dropout Prediction Model',
-      image: '../images/dropout.jpeg', 
+      image: '../images/dropout.jpeg',
       description: 'A machine learning model predicting the risk of students dropping out of university based on various features.',
       keywords: 'ML, Python',
       github: 'https://github.com/allalakhayri/Dropout-prediction-model-',
-      demo: 'https://drive.google.com/file/d/1exGux1EymYRyiki5jlx0cYa5E_2OfxN0/view?usp=sharing', 
+      demo: 'https://drive.google.com/file/d/1exGux1EymYRyiki5jlx0cYa5E_2OfxN0/view?usp=sharing',
     },
   ];
 
